5f6f24464039f500224af9c88b57b3156756bb94
SolRobot <Altair_SA@mail.ru>
2026-02-21 11:29:02 +0300

    Занятие по Vue JS

---
+++ script.js
@@ -0,0 +1,37 @@
+let vm = new Vue({
+    el: "#app",
+    data: { message: "Hello, Vue" }
+})
+let vm01 = new Vue({
+    el: "#app01",
+    data: {message: "Текст"}     
+            
+})
+let vm1 = new Vue({
+    el:"#app1",
+    data: {checked: false}
+})
+
+let vm2 = new Vue({          
+    el: '#app2',
+    data: { picked: 'Second'}
+
+})
+let vm3 = new Vue({
+    el: '#app3',
+    data: { language: 'Python' }
+})
+let vm4 = new Vue({
+    el: "#app4",
+    data: { n: 1,
+        checked: true,
+        message: "Разворот"
+    }
+})
+let vm5 = new Vue({
+    el: "#app5",
+    data: {
+        query: "голый землекоп"
+    }
+})
+        
